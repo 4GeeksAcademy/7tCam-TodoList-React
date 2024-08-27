ec70bdcafaf8c680fc126f64ce64c07bbf4df15e
luisgomez7t <162279891+7tCam@users.noreply.github.com>
2024-08-27 13:48:26 +0000

    finalFinal

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -54,34 +54,35 @@ const Home = () => {
 	// or (event) => setdatos(event.target.input.value)
 	return (
 		<div className=" py-4 mx-auto w-75 d-flex flex-column justifyu-content-center vh-100" style={{ backgroundColor: '#e7efe9' }}>
-			<h1 className="py-3 text-center fw-light " style={{ fontSize: '8rem', color: '#b7c1ba' }}>todos</h1>
-			<div className="bg-white border-bottom border-start border-end border-1 rounded-1 border-dark  mx-auto mb-1 w-50"  >
+			<h1 className="py-3 text-center fw-light " style={{ fontSize: '8rem', color: '#b7c1ba' }}>Lista de tareas</h1>
+			<div className="bg-white border border-1 rounded-1 border-dark  mx-auto mb-1 w-50"  >
 				<div className="bg-white border-bottom  border-1 rounded-1 border-dark  mx-auto mb-1" >
 					<div className="bg-white  border-bottom border-top  border-1 rounded-1 border-dark  mx-auto mb-1">
 						<form className="w-100 mx-auto d-flex flex-column justify-content-center" action="" onSubmit={enviarDatos}>
 							{/* asignar el evento onChange para vigilar los cambios */}
-							<input placeholder="What needs to be done?" className="border-bottom w-100 form-control-plaintext py-2  text-center" type="text" id="lol" />
+							<input placeholder="What needs to be done?" className="mt-2 w-75 form-control-plaintext py-2  mx-auto ml-4" type="text" id="lol" />
+							<hr />
 
 						</form>
 						{/* ya que ul no captura informacion,debe estar fuera de form */}
 						{/* condicion por si no hay tareas */}
 						{tareas.length === 0 ? (
-							<p className="text-center my-2">No hay tareas, añadir tareas</p>
+							<p className="text-center my-0 pb-3 border-bottom">No hay tareas, añadir tareas</p>
 						) : (tareas.map((tarea, i) => (
-							<ul className="list-unstyled w-100 mx-auto " key={i} onMouseEnter={() => setHoveredTarea(i)}
+							<ul className="list-unstyled w-100 mx-auto mb-0 " key={i} onMouseEnter={() => setHoveredTarea(i)}
 								onMouseLeave={() => setHoveredTarea(null)}>
 								{/* saque id={i}  xq key es la identificacion como un id */}
-								<li className="w-100 border-bottom text-center my-2"> {tarea} {hoveredTarea === i && ( // Condición para mostrar el botón cuando el ratón está sobre la tarea
-									<a onClick={() => borrar(i)}><i className="fa-solid fa-xmark"></i></a>
-								)} </li>
+								<li className="w-100  position-relative  my-0 "> {tarea} {hoveredTarea === i && ( // Condición para mostrar el botón cuando el ratón está sobre la tarea
+									<a className="position-absolute" onClick={() => borrar(i)}><i className=" fa-solid fa-xmark"></i></a>
+								)} </li> <hr />
 							</ul>)))
 						}
 						{/* Mostrar el contador de tareas */}
-						<p className="text-start py-2  border-top mb-0" >Total de tareas: {tareas.length}</p>
+						<p className="text-start py-2  ms-3 mb-2" >Total de tareas: {tareas.length}</p>
 					</div>
 				</div>
 
-			</div>
+			 </div>
 
 
 
